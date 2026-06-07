0376730d3b615017a6270e44bfc36b71c242b3d3
euics <pjh575157@gmail.com>
2026-06-07 14:17:24 +0900

    feat: 질문 정보 조회 시 OPTIONS TIME 필드 삭제 작업

--- app/options/schema/response/options_response.py
+++ app/options/schema/response/options_response.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from typing import TYPE_CHECKING
 
 from pydantic import BaseModel
@@ -11,8 +10,6 @@ class GetOptionResponse(BaseModel):
     options_seq: int
     question_seq: int
     content: str
-    created_at: datetime
-    updated_at: datetime
     vote_count: int | None = None
 
     @classmethod
@@ -25,7 +22,5 @@ class GetOptionResponse(BaseModel):
             options_seq=option.options_seq,
             question_seq=option.question_seq,
             content=option.content,
-            created_at=option.created_at,
-            updated_at=option.updated_at,
             vote_count=vote_count,
         )
