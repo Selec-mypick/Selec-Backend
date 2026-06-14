549cc420f5c6e73d23fd32b452ad72cd4fb82649
euics <pjh575157@gmail.com>
2026-06-14 15:54:05 +0900

    feat: Gemini 투표 생성 시간 수정 작업 0100

--- scheduler/jobs/question_invite_all/config.py
+++ scheduler/jobs/question_invite_all/config.py
@@ -6,7 +6,7 @@ JOB_SCHEDULE = JobSchedule(
     job_id="question_invite_all",
     job_name="매일 새벽 1시 Batch 질문 전체 사용자 초대",
     timezone=settings.timezone,
-    hour=15,
-    minute=53,
+    hour=1,
+    minute=0,
     second=0,
 )
