d23f83e173e2d2f35c4329f99a839047f3ca65f5
euics <pjh575157@gmail.com>
2026-06-14 15:17:57 +0900

    feat: Gemini 투표 생성 시간 수정 작업 2330

--- scheduler/jobs/gemini_vote_create/config.py
+++ scheduler/jobs/gemini_vote_create/config.py
@@ -4,9 +4,9 @@ from scheduler.job_schedule import JobSchedule
 
 JOB_SCHEDULE = JobSchedule(
     job_id="gemini_vote_create",
-    job_name="매일 자정 Gemini 투표 생성",
+    job_name="매일 밤 11시 30분 Gemini 투표 생성",
     timezone=settings.timezone,
-    hour=0,
-    minute=0,
+    hour=23,
+    minute=30,
     second=0,
 )
